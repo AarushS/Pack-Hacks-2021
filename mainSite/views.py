102521083207ae155e0c0cd64d253492cbba9544
mrchhaya <mohitchhaya24@gmail.com>
2021-03-20 14:24:13 -0400

    Initial Website Push

--- mainSite/views.py
+++ mainSite/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+class Home(View):
+    def get(self, *args, **kwargs):
+        context = {"pageName":"Home"}
+        return render(self.request, "mainSite/home.html",context)
